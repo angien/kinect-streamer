97fa355be9a3bb60d78398fe2fe15d470864c7ee
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2014-11-08 20:10:54 -0800

    Initial commit of TextToSpeech module.

---
+++ TextToSpeech/TextToSpeech/Driver.cs
@@ -0,0 +1,11 @@
+﻿namespace TextToSpeech
+{
+    class Driver
+    {
+        static void Main( string[] args )
+        {
+            SpeechOutput speaker = new SpeechOutput();
+            speaker.OutputToAudio( "I'm hungry for brains" );
+        }
+    }
+}
---
+++ TextToSpeech/TextToSpeech/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TextToSpeech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TextToSpeech")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef3fd8fd-eefb-408d-8eb3-1b8fa42b01ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
