b533fb7ab5f2b374c895ef6c10f56222336747c7
unknown <jjc053@ACS-CSEB270-43.ad.ucsd.edu>
2014-12-04 00:09:52 -0800

    Updated Enrollment GUI

---
+++ EnrollmentGUI/simlips/simlips/Page4.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace simlips
+{
+    /// <summary>
+    /// Interaction logic for Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page3());
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+    }
+}
